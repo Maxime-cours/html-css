2658161205f4df69c071cd85b361cb301782522a
Maxime Perraud <contact@maxime-p.dev>
2024-01-20 22:27:46 +0100

    initial commit

---
+++ docs/css/intro.md
@@ -0,0 +1,4 @@
+---
+title: CSS Introduction
+slug: /css/introduction
+---
---
+++ docusaurus.config.ts
@@ -0,0 +1,62 @@
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
+
+const config: Config = {
+  title: 'Maxime Cours',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://cours.maxime-p.dev',
+  baseUrl: '/',
+
+  organizationName: 'Maxime-cours',
+  projectName: 'html-css',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/Maxime-cours/html-css/edit/main/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    navbar: {
+      title: 'Maxime-cours',
+      items: [
+        {
+          href: 'https://github.com/Maxime-cours/html-css',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+}
+
+export default config
